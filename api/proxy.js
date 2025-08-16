2fadcd052f3d21360a40fcf0757f6561e8bf389f
Sabbir Hossain <36847136+rssabbirdev@users.noreply.github.com>
2025-08-16 14:02:44 +0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
-  const { mobile, startISO, endISO } = req.query;
+  const { mobile, startISO, endISO, ipAddress, deviceDetails } = req.query;
   const scriptID = 'AKfycbx_XTWnCkdoay0HA_Kr7GTQGVInCLsRR467Z6kvz2R9pCYZyX5x1Z_JJl2tP323joCD'
 
-  const url = `https://script.google.com/macros/s/${scriptID}/exec?action=getAttendanceDataByMobile&mobile=${mobile}&startISO=${startISO}&endISO=${endISO}`;
+  const url = `https://script.google.com/macros/s/${scriptID}/exec?action=getAttendanceDataByMobile&mobile=${mobile}&startISO=${startISO}&endISO=${endISO}&deviceDetails=${deviceDetails}&ipAddress=${ipAddress}`;
 
   const response = await fetch(url);
   const data = await response.json();
